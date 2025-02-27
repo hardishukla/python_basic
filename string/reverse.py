47b27d753d8c2af7c90a98665dfb160f5781a82e
hardishukla <155943005+hardishukla@users.noreply.github.com>
2025-02-27 13:03:25 +0530

    Create reverse.py

---
+++ string/reverse.py
@@ -0,0 +1,4 @@
+#take input of user name and print it in reverse order
+name = input("Enter your name: ")#taking input from user
+name2 = print(name[::-1])#printing the name in reverse order
+print("your reversed name is", name2) # output of the reversed name
