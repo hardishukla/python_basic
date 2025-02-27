803e41fcfcecd02e2e9ffe91ae49a9e67b521821
hardishukla <155943005+hardishukla@users.noreply.github.com>
2025-02-27 12:42:09 +0530

    Create leap_year.py

---
+++ leap_year.py
@@ -0,0 +1,6 @@
+# check wheather given year is a leap year or not 
+year =int(input("enter year :"))#input from user
+if year % 4 == 0: #logic
+    print("leap year")
+else :
+    print("non leap year")
