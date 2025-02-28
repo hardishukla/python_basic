59c603a83a0db992ec188807d903ebeb87d43b99
hardishukla <155943005+hardishukla@users.noreply.github.com>
2025-02-28 14:39:12 +0530

    Create string_to_int.py

---
+++ datatype/string_to_int.py
@@ -0,0 +1,6 @@
+#convert string representation of number into integer and float
+string =input("Enter a number:")
+integer = int(string)
+floating = float(string)
+print("Integer:",integer)
+print("Floating point:",floating)
