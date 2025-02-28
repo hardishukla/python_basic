1f9b50473f033fda5b6e0d88881a2b5565cde808
hardishukla <155943005+hardishukla@users.noreply.github.com>
2025-02-28 13:08:56 +0530

    Update replaceword.py

--- string/replaceword.py
+++ string/replaceword.py
@@ -1,5 +1,6 @@
-noob =input("enter the sentence")
-pro= input("enter the character to be replaced")
-hack = input("enter the new character")
-replace = noob.replace(pro, hack)
+#replace a character in a string
+noob =input("enter the sentence ")#input the orignal sentence
+pro= input("enter the character to be replaced")#input the character to be replaced
+hack = input("enter the new character")#input the new character
+replace = noob.replace(pro, hack)#logic to replace the character
 print(replace)
