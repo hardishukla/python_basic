3303e3f59a7ca47fb4cee9a2c7463b7308734464
hardishukla <155943005+hardishukla@users.noreply.github.com>
2025-02-28 15:03:31 +0530

    Create int_or_float.py

---
+++ datatype/int_or_float.py
@@ -0,0 +1,6 @@
+#write a program to check whether a given number is integer or float
+num =float(input("Enter a number: ")) # input a number  
+if num == int(num): # check if the number is an integer
+    print("The number is an integer") # print the result
+else: # if the number
+    print("The number is a float") # print the result
