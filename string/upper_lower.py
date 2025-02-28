81feae7678c616d55ffa59e30e661af1eb962b31
hardishukla <155943005+hardishukla@users.noreply.github.com>
2025-02-28 13:21:59 +0530

    Create upper_lower.py

---
+++ string/upper_lower.py
@@ -0,0 +1,6 @@
+#convert given string into upper and lower case
+sentence= input("enter your sentence")#input from user
+upper=sentence.upper()#converts into upper case
+lower= sentence.lower()#converts into lower case
+print(upper) 
+print(lower)
