4fc435f0a406d189b378b3a84c23703620e0e480
hardishukla <155943005+hardishukla@users.noreply.github.com>
2025-03-05 13:32:18 +0530

    Create sum.py

---
+++ list/sum.pyx
@@ -0,0 +1,4 @@
+#write a list and print its sum
+num = list(map(int, input("enter the numbers: ").split())) #taking input from user
+sm = sum(num) #sum of the list
+print(sm) #printing the sum of the list2
