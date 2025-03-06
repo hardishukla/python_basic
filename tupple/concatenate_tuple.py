323b6943cbfdc487b158b9f15dfa62b87b3c5327
hardishukla <155943005+hardishukla@users.noreply.github.com>
2025-03-06 15:05:07 +0530

    Create concatenate_tuple.py

---
+++ tupple/concatenate_tuple.py
@@ -0,0 +1,5 @@
+#concatenate two tuples and print the result
+num = tuple(map(int, input("enter the tuple 1: ").split()))#input 1
+num1 = tuple(map(int, input("enter the tuple 2: ").split()))#input 2
+result = num + num1 #add
+print(result)
