22b39baf38ced5d6dd28898d8329c408fe394f24
hardishukla <155943005+hardishukla@users.noreply.github.com>
2025-03-07 14:10:02 +0530

    Create finding.py

---
+++ dictionary/finding.py
@@ -0,0 +1,8 @@
+# Create a dictionary with three key-value pairs and print all keys and values.
+dictonary = {
+    "name": "charmi",
+    "age": "25",
+    "city": "navi mumbai"
+}
+print(dictonary.keys()) #print key
+print(dictonary.values())#print values
